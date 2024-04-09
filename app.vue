9576ac8fb61ee1a6b97d036392dbd2f77da77822
Hari <22537086+song52wow@users.noreply.github.com>
2024-04-09 16:11:07 +0800

    style: Update keyword

--- app.vue
+++ app.vue
@@ -3,25 +3,27 @@ const title = "ATELIER GO RUSTIC";
 const description =
   'Explore handcrafted home goods at Atelier GO RUSTIC, blending the serene aesthetics of "wabi-sabi" with natural materials and minimal human intervention. Discover our collection of artisanal pieces, meticulously crafted to harmonize with your living space and evoke tranquility.';
 const keyword = [
+  title,
+  title.toLowerCase(),
   "Wabisabi style",
   "Quercus acutissima",
   "Weathered wood",
-  "Table tray",
-  "Imperfectus style",
-  "Pottery",
-  "Copper tea set",
-  "Handcrafted",
-  "Rustic style",
-  "Natural materials",
-  "Creative workshop",
-  "Handmade decor",
-  "Exquisite craftsmanship",
-  "Wood design",
-  "Natural aesthetics",
-  "Hand carving",
-  "Simple living",
-  "Handcrafted production",
-  "Primitive style",
+  // "Table tray",
+  // "Imperfectus style",
+  // "Pottery",
+  // "Copper tea set",
+  // "Handcrafted",
+  // "Rustic style",
+  // "Natural materials",
+  // "Creative workshop",
+  // "Handmade decor",
+  // "Exquisite craftsmanship",
+  // "Wood design",
+  // "Natural aesthetics",
+  // "Hand carving",
+  // "Simple living",
+  // "Handcrafted production",
+  // "Primitive style",
 ];
 
 useSeoMeta({
@@ -54,8 +56,13 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="w-100vw h-100vh flex items-center justify-center">
-    <h1 ref="mainRef" class="text-8vw">Coming soon</h1>
+  <div class="w-100vw h-100vh relative">
+    <h1 class="text-center">{{ title }}</h1>
+
+    <div class="absolute top-50% left-50% -translate-50% ws-nowrap">
+
+      <strong ref="mainRef" class="text-8vw">Coming soon</strong>
+    </div>
   </div>
 </template>
 
